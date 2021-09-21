9727bdbc691b182073afaff598a51c43d363e15a
Anton Ludvigsen <s215815@dtu.dk>
2021-09-21 12:10:42 +0200

    Første iteration. Enkelt player spiller til 40

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -1,5 +1,26 @@
+import java.util.*;
 public class DiceGame {
     public static void main(String[] args) {
         System.out.println("test");
+        int total = 0;
+        while (total<40) {
+                int max = 6;
+                int min = 1;
+                int terning1 = (int) Math.floor(Math.random() * (max - min + 1) + min);
+                int terning2 = (int) Math.floor(Math.random() * (max - min + 1) + min);
+                System.out.println("De to terningekast giver " + terning1 + " og " + terning2);
+                System.out.println("Sammenlagt har du slået " + (terning1 + terning2));
+                total = total + terning1 + terning2;
+                if (total < 40) {
+                    System.out.println("Dit nye total er " + total + ". Vil du slå igen? ");
+                }
+                else {
+                    System.out.println("Dit total bliver "+total+". Tillykke, du vandt!");
+                    break;
+                }
+                new Scanner(System.in).nextLine();
+
+        }
+
     }
 }
