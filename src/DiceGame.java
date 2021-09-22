6d9e8f97071a425b2fa713f597c85395f4e7f389
yech199 <silja.sandersen@hotmail.com>
2021-09-22 15:27:42 +0200

    Første version der virker med 2 players

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -8,7 +8,7 @@ public class DiceGame {
         Player player1 = new Player(0);
         Player player2 = new Player(0);
 
-        while (player1.score <= 40 || player2.score <= 40) { // Bruger et while loop til at blive ved med at kaste terninger
+        while (player1.score < 40 && player2.score < 40) { // Bruger et while loop til at blive ved med at kaste terninger
             int terning1 = terningx.roll();
             // Bruger terningens funktion roll, som er angivet inde i Dice klassen
             int terning2 = terningy.roll();
@@ -20,22 +20,21 @@ public class DiceGame {
             System.out.println("Sammenlagt har Player 2 slået " + (terning3 + terning4));
 
             player1.score = player1.score + terning1 + terning2; // Total bliver ved med at forstørres,
-            player2.score = player2.score + terning3 + terning4;
-            // indtil while loopet stopper
+            player2.score = player2.score + terning3 + terning4; // indtil while loopet stopper
 
 
-            if (player1.score == 2){ // Resetter total hvis 2 er slået
+            if (terning1 + terning2 == 2){ // Resetter total hvis 2 er slået
                 player1.score = 0;
-                System.out.println("Sammenlagt har Player 1 nu en score på " + player1.score + " point.");
+                System.out.println("Player 1 har nu " + player1.score + " point.");
             }
-            if (player2.score == 2){ // Resetter total hvis 2 er slået
+            if (terning3 + terning4 == 2){ // Resetter total hvis 2 er slået
                 player2.score = 0;
-                System.out.println("Sammenlagt har Player 2 nu en score på " + player2.score + " point.");
+                System.out.println("Player 2 har nu " + player2.score + " point.");
             }
 
-            if (player1.score < 40 || player2.score < 40) {
-                System.out.println("Player 1 har en score på " + player1.score);
-                System.out.println("Player 2 har en score på " + player2.score);
+            if (player1.score < 40 && player2.score < 40) {
+                System.out.println("Sammenlagt har Player 1 nu en score på " + player1.score);
+                System.out.println("Sammenlagt har Player 2 nu en score på " + player2.score);
                 System.out.println("Tryk enter for at slå med terningerne igen");
             } else if (player1.score >= 40){
                 System.out.println("Player 1 har vundet. Din totale score bliver " + player1.score + ". Tillykke, du vandt!");
