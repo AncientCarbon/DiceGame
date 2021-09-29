54c655ec73bcd4a79cdbfcd5da26d92f85c0378a
Anton Ludvigsen <s215815@dtu.dk>
2021-09-29 22:54:01 +0200

    tilføjet mere dataindsamling til DiceTest

--- src/DiceTest.java
+++ src/DiceTest.java
@@ -11,6 +11,12 @@ public class DiceTest {
 
         int total1 = 0;
         int total2 = 0;
+        int face1 = 0;
+        int face2 = 0;
+        int face3 = 0;
+        int face4 = 0;
+        int face5 = 0;
+        int face6 = 0;
 
         for(int t = 0; t < 1000; t++){
             int t1 = terning1.roll();
@@ -19,8 +25,50 @@ public class DiceTest {
 
             total1 += t1;
             total2 += t2;
+
+            if (t1 == 1 || t2 == 1){
+                if (t1 == 1 && t2 == 1){
+                    face1 += 2;
+                }
+                else face1 += 1;
+            }
+            if (t1 == 2 || t2 == 2){
+                if (t1 == 2 && t2 == 2){
+                    face2 += 2;
+                }
+                else face2 += 1;
+            }
+            if (t1 == 3 || t2 == 3){
+                if (t1 == 3 && t2 == 3){
+                    face3 += 2;
+                }
+                else face3 += 1;
+            }
+            if (t1 == 4 || t2 == 4){
+                if (t1 == 4 && t2 == 4){
+                    face4 += 2;
+                }
+                else face4 += 1;
+            }
+            if (t1 == 5 || t2 == 5){
+                if (t1 == 5 && t2 == 5){
+                    face5 += 2;
+                }
+                else face5 += 1;
+            }
+            if (t1 == 6 || t2 == 6){
+                if (t1 == 6 && t2 == 6){
+                    face6 += 2;
+                }
+                else face6 += 1;
+            }
         }
         System.out.println("Terning 1 total: " + total1);
         System.out.println("Terning 2 total: " + total2);
+
+        System.out.println("\nAntal fra hver side:\n 1'ere: " + face1 + "\n 2'ere: " + face2 +
+                "\n 3'ere: " + face3 + "\n 4'ere: " + face4 + "\n 5'ere: " + face5 + "\n 6'ere: " + face6);
+        int kast = face1 + face2 + face3 + face4 + face5 + face6;
+        System.out.println("\nAntal kast i det hele: " + (kast/2));
     }
 }
