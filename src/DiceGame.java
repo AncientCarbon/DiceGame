51b0c7211e7c2230a04980ca45b4ccb19e9058c5
Anton Ludvigsen <s215815@dtu.dk>
2021-09-30 14:39:37 +0200

    Kommentarer til koden

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -128,7 +128,7 @@ public class DiceGame {
                     player2.score += terning3 + terning4;
                     if (terning3 + terning4 == 12) {
                         System.out.println("Du vandt, " + player2.name); //der skal kastes igen, hvis det bliver 12 igen vinder man
-                        break;
+                        System.exit(0);
                     }
                 } else if ((terning3 + terning4) < 11) {
 
--- src/DiceTest.java
+++ src/DiceTest.java
@@ -1,4 +1,4 @@
-//version 1.1.0
+//version 1.1.1
 import java.util.*;
 public class DiceTest {
     public static void main(String[] args) {
@@ -6,19 +6,20 @@ public class DiceTest {
         Dice terning2 = new Dice(6);
 
         System.out.println("Forventet totalværdi: ~3500");
+        System.out.println("Forventet antal kast per terning");
         System.out.print("Tryk enter for at kaste 1000 gange: ");
         new Scanner(System.in).nextLine();
 
-        int total1 = 0;
+        int total1 = 0; // tjekker hvad den totale værdi af alle terning1 og terning2 kast er
         int total2 = 0;
-        int face1 = 0;
+        int face1 = 0; // face1-6 bruges til at optælle antal af slag af hver øjenværdi.
         int face2 = 0;
         int face3 = 0;
         int face4 = 0;
         int face5 = 0;
         int face6 = 0;
 
-        for(int t = 0; t < 1000; t++){
+        for(int t = 0; t < 1000; t++){ // loop kører 1000 gange præcis
             int t1 = terning1.roll();
             int t2 = terning2.roll();
             System.out.println(t1 + ", " + t2);
@@ -26,11 +27,11 @@ public class DiceTest {
             total1 += t1;
             total2 += t2;
 
-            if (t1 == 1 || t2 == 1){
-                if (t1 == 1 && t2 == 1){
+            if (t1 == 1 || t2 == 1){ // if statement tjekker om der er blevet slået 1
+                if (t1 == 1 && t2 == 1){ //hvis begge kast er 1 tilføjer den 2 til antallet af 1'ere slået
                     face1 += 2;
                 }
-                else face1 += 1;
+                else face1 += 1; //ellers hvis kun 1 af terningerne er 1, tilføjer den 1 til antallet af 1'ere.
             }
             if (t1 == 2 || t2 == 2){
                 if (t1 == 2 && t2 == 2){
@@ -66,9 +67,9 @@ public class DiceTest {
         System.out.println("Terning 1 total: " + total1);
         System.out.println("Terning 2 total: " + total2);
 
-        System.out.println("\nAntal fra hver side:\n 1'ere: " + face1 + "\n 2'ere: " + face2 +
-                "\n 3'ere: " + face3 + "\n 4'ere: " + face4 + "\n 5'ere: " + face5 + "\n 6'ere: " + face6);
+        System.out.println("\nAntal fra hver side:\n1'ere: " + face1 + "\n2'ere: " + face2 +
+                "\n3'ere: " + face3 + "\n4'ere: " + face4 + "\n5'ere: " + face5 + "\n6'ere: " + face6);
         int kast = face1 + face2 + face3 + face4 + face5 + face6;
-        System.out.println("\nAntal kast i det hele: " + (kast/2));
+        System.out.println("\nAntal kast i det hele: " + (kast/2)); //dividerer med 2 fordi der kastes 2 terninger pr kast
     }
 }
